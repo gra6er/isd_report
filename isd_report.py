1b4e19d1d8e4a0d429dcee979ce23947fcb36fe0
akazyulin <akazyulin@i-core.ru>
2022-02-04 16:20:14 +0300

    code refactoring, revision of reporting parameters

--- isd_report.py
+++ isd_report.py
@@ -1,8 +1,19 @@
 from jira import JIRA
 import pprint
 import os
+from datetime import datetime
+import time
+import pandas as pd
+
 
 pp = pprint.PrettyPrinter(width=41, compact=True)
+
+isd_members = [
+    'msimonyants',
+    'skorotkih',
+    'ruteshev',
+    'ialdoshin'
+]
 
 def print_issue(issue):
     """
@@ -42,6 +53,38 @@ def get_JQL(project = 'ISD', custom = None, assignee = None, created_from = None
     return jql
 
 
+def get_incident_issues_JQL(timepoint_from, timepoint_to, breached = None):
+    if breached == False: 
+        return get_JQL(custom = '"Время на решение" != breached()', created_from=timepoint_from, created_to=timepoint_to, issue_type='Инцидент', status='Закрыта')
+    else:
+        return get_JQL(created_from=timepoint_from, created_to=timepoint_to, issue_type='Инцидент', status='Закрыта')
+
+
+def get_none_incident_issues_JQL(timepoint_from, timepoint_to, breached = None):
+    if breached == False: 
+        return get_JQL(custom = '"Время до первого отклика" != breached()', created_from=timepoint_from, created_to=timepoint_to, issue_type='"Запрос на обслуживание с заверениями", Изменение', status='Закрыта')
+    else:
+        return get_JQL(created_from=timepoint_from, created_to=timepoint_to, issue_type='"Запрос на обслуживание с заверениями", Изменение', status='Закрыта')
+
+
+def get_customfield_time(issue, customfield):
+    # TODO write IndexError handling by finding reaction time in issue history
+    try:
+        time = issue.raw['fields'][customfield]['completedCycles'][0]['elapsedTime']['millis']
+        return time
+    except IndexError:
+        raise
+
+def avg_customfield_time(issues, customfield):
+    sum = 0
+    for issue in issues:
+        try:
+            sum += get_customfield_time(issue, customfield)
+        except:
+            issues.total -= 1
+   
+    return sum / issues.total
+
 def parameter_1(jira, timepoint_from, timepoint_to):
     """
     Count 1st reporting parameter: Share of timely processed requests
@@ -55,12 +98,12 @@ def parameter_1(jira, timepoint_from, timepoint_to):
 
     # and type = Инцидент and priority = Безотлагательный
 
-    issues = jira.search_issues(f'project = ISD and "Время до первого отклика" != breached() and created >= \'{timepoint_from}\' and created < \'{timepoint_to}\'', maxResults=1)
+    issues = jira.search_issues(get_none_incident_issues_JQL(timepoint_from, timepoint_to, breached=False), maxResults=1)
     not_breached_count = issues.total
-    issues = jira.search_issues(f'project = ISD and created >= \'{timepoint_from}\' and created < \'{timepoint_to}\'', maxResults=1)
+    issues = jira.search_issues(get_none_incident_issues_JQL(timepoint_from, timepoint_to), maxResults=1)
     all_count = issues.total
 
-    print(f'1. not_breached: {not_breached_count}  all: {all_count}')
+    # print(f'1. not_breached: {not_breached_count}  all: {all_count}')
 
     return not_breached_count / all_count * 100
 
@@ -78,12 +121,12 @@ def parameter_2(jira, timepoint_from, timepoint_to):
 
     # and type = Инцидент and priority = Безотлагательный
 
-    issues = jira.search_issues(get_JQL(custom = '"Время на решение" != breached()', created_from=timepoint_from, created_to=timepoint_to, issue_type='Инцидент', status='Закрыта'), maxResults=1)
+    issues = jira.search_issues(get_incident_issues_JQL(timepoint_from, timepoint_to, breached=False), maxResults=1)
     not_breached_count = issues.total
-    issues = jira.search_issues(get_JQL(created_from=timepoint_from, created_to=timepoint_to, issue_type='Инцидент', status='Закрыта'), maxResults=1)
+    issues = jira.search_issues(get_incident_issues_JQL(timepoint_from, timepoint_to), maxResults=1)
     all_count = issues.total
 
-    print(f'2. not_breached: {not_breached_count}  all: {all_count}')
+    # print(f'2. not_breached: {not_breached_count}  all: {all_count}')
 
     return not_breached_count / all_count * 100
 
@@ -97,40 +140,45 @@ def parameter_7(jira, timepoint_from, timepoint_to):
         Return:
             int -- percentage of requests processed on time
     """
-    issues = jira.search_issues(f'project = ISD and status = Закрыта and created >= \'{timepoint_from}\' and created < \'{timepoint_to}\'', maxResults=False, fields=['customfield_10125', 'customfield_10401'])
+    # start_time = datetime.now()
 
-    print(len(issues))
+    df = pd.DataFrame(columns=["Инцидент (откл.)", "Инцидент (реш.)",  "Изменение", "Обслуживание"], index=isd_members)
 
-    # TODO Узнать как это обрабатывать
-    # --------------------------------
+    for member in isd_members:
 
-    total = issues.total
-    total_10125 = total
-    total_10401 = total
-    millis_sum_10125 = 0
-    millis_sum_10401 = 0
-    excluded_count_10125 = 0
-    excluded_count_10401 = 0
-    for issue in issues:
-        if len(issue.raw['fields']['customfield_10125']['completedCycles']) == 0:
-            total_10125 -= 1
-            excluded_count_10125 += 1  
-        else:
-            millis_sum_10125 += issue.raw['fields']['customfield_10125']['completedCycles'][0]['elapsedTime']['millis']
-    
-        if len(issue.raw['fields']['customfield_10401']['completedCycles']) == 0:
-                total_10401 -= 1
-                excluded_count_10401 += 1
-        else:
-            millis_sum_10401 += issue.raw['fields']['customfield_10401']['completedCycles'][0]['elapsedTime']['millis']
-    
+        # Fill Инцидент (откл.) column
+        incident_issues_10125 = jira.search_issues(get_JQL(created_from=timepoint_from, created_to=timepoint_to, status='Закрыта', issue_type='Инцидент', assignee=member) \
+            , maxResults=False, fields=['customfield_10125']) 
 
-    print(f"Excluded count (10125): {excluded_count_10125}")
-    print(f"Excluded count (10401): {excluded_count_10401}")
+        incident_avg_time_10125  = avg_customfield_time(incident_issues_10125, 'customfield_10125')
+        df.loc[member, 'Инцидент (откл.)'] = incident_avg_time_10125 / 60000
 
-    millis_mean_10125 = millis_sum_10125 / total_10125
-    millis_mean_10401 = millis_sum_10401 / total_10401
-    return (millis_mean_10125 / 1000, millis_sum_10401 / 1000)
+        # Fill Инцидент (реш.) column
+        incident_issues_10401 = jira.search_issues(get_JQL(created_from=timepoint_from, created_to=timepoint_to, status='Закрыта', issue_type='Инцидент', assignee=member) \
+            , maxResults=False, fields=['customfield_10401'])
+
+        incident_avg_time_10401  = avg_customfield_time(incident_issues_10401, 'customfield_10401')
+        df.loc[member, 'Инцидент (реш.)'] = incident_avg_time_10401 / 60000
+
+        # Fill Изменение column
+        change_issues = jira.search_issues(get_JQL(created_from=timepoint_from, created_to=timepoint_to, status='Закрыта', issue_type='Изменение', assignee=member) \
+            , maxResults=False, fields=['customfield_10125'])
+
+        change_avg_time  = avg_customfield_time(change_issues, 'customfield_10125')
+
+        df.loc[member, 'Изменение'] = change_avg_time / 60000
+
+        # Fill Обслуживание column
+        service_issues = jira.search_issues(get_JQL(created_from=timepoint_from, created_to=timepoint_to, status='Закрыта', issue_type='"Запрос на обслуживание с заверениями"', assignee=member) \
+            , maxResults=False, fields=['customfield_10125'])
+
+        service_avg_time  = avg_customfield_time(service_issues, 'customfield_10125')
+
+        df.loc[member, 'Обслуживание'] = service_avg_time / 60000
+
+    # print(f'\n time for 7 param: {datetime.now() - start_time}')
+
+    return df
 
 
 jira_host = os.getenv('JIRA_URL')
@@ -142,8 +190,13 @@ jira = JIRA(jira_host, basic_auth=(jira_user, jira_password))
 timepoint_from = os.getenv('TIMEPOINT_FROM')
 timepoint_to = os.getenv('TIMEPOINT_TO')
 
-# print(parameter_1(jira, timepoint_from, timepoint_to))
-print(parameter_2(jira, timepoint_from, timepoint_to))
-# print(parameter_7(jira, timepoint_from, timepoint_to))
+print("1. Доля своевременно обработанных запросов (в %):")
+print(parameter_1(jira, timepoint_from, timepoint_to))
 
-# print(get_JQL(custom = '"Время на решение" != breached()', created_from=timepoint_from, created_to=timepoint_to, issue_type='Инцидент'))
+print("\n2. Доля решенных обработанных запросов (в %):")
+print(parameter_2(jira, timepoint_from, timepoint_to))
+
+print("\n7. Среднее время обработки запросов различных типов заявок (в минутах):")
+print(parameter_7(jira, timepoint_from, timepoint_to))
+
+# print(get_JQL(custom = '"Время до первого отклика" != breached()', created_from=timepoint_from, created_to=timepoint_to, status='Закрыта', issue_type='"Запрос на обслуживание с заверениями", Изменение', assignee=", ".join(isd_members)))
